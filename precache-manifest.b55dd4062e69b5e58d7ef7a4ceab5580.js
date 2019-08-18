73a5435a7cf9f59eec68fd8c7d3f5ba898fa952b
mjperezg2010 <mjperezg2010@unitec.edu>
2019-08-18 11:48:40 -0600

    Updates

---
+++ precache-manifest.b55dd4062e69b5e58d7ef7a4ceab5580.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc31b7f375db242710541d3dc2adc09e",
+    "url": "/examen1UX/index.html"
+  },
+  {
+    "revision": "2b1469f719446ff2dc1a",
+    "url": "/examen1UX/static/css/main.b36729f9.chunk.css"
+  },
+  {
+    "revision": "6ca3ae044219c611df1e",
+    "url": "/examen1UX/static/js/2.cf42f021.chunk.js"
+  },
+  {
+    "revision": "2b1469f719446ff2dc1a",
+    "url": "/examen1UX/static/js/main.2d99afa0.chunk.js"
+  },
+  {
+    "revision": "0ce57483a19329687c9a",
+    "url": "/examen1UX/static/js/runtime~main.d0c8d869.js"
+  }
+]);
